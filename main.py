8fc073aa94b9b5c14b53b14a4b51b65c187a270a
Hasan100sad <hasan.sad24@gmail.com>
2026-06-27 00:26:53 +0330

    Update main.py
    Update inti fail maine

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import requests
 from telegram import Update
 from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
-# توکن‌ها از Environment Variables
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
 BALE_TOKEN = os.environ.get("BALE_TOKEN")
 BALE_CHAT_ID = os.environ.get("BALE_CHAT_ID")
@@ -11,7 +10,7 @@ BALE_CHAT_ID = os.environ.get("BALE_CHAT_ID")
 BALE_API_URL = "https://tapi.bale.ai/bot{}/sendMessage"
 
 
-async def forward_to_bale(update: Update, context: ContextTypes.DEFAULT_TYPE):
+def forward_to_bale(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if update.message and update.message.text:
         text = update.message.text
 
@@ -24,14 +23,15 @@ async def forward_to_bale(update: Update, context: ContextTypes.DEFAULT_TYPE):
         )
 
 
-async def main():
+def main():
     application = Application.builder().token(TELEGRAM_TOKEN).build()
+
     application.add_handler(
         MessageHandler(filters.TEXT & ~filters.COMMAND, forward_to_bale)
     )
-    await application.run_polling()
+
+    application.run_polling()
 
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    main()
