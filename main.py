21afca45198037c6f3e4a6e229fd787dccc83950
Hasan100sad <hasan.sad24@gmail.com>
2026-06-26 23:26:27 +0330

    Update main.py
    inti main faile

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
 import os
-import time
-from flask import Flask
+import requests
+from telegram import Update
+from telegram.ext import Application, MessageHandler, filters, ContextTypes
 
-app = Flask(__name__)
+# توکن‌ها از Environment Variables
+TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN")
+BALE_TOKEN = os.environ.get("BALE_TOKEN")
+BALE_CHAT_ID = os.environ.get("BALE_CHAT_ID")
 
-@app.route("/")
-def home():
-    return "Bot is alive"
+BALE_API_URL = "https://tapi.bale.ai/bot{}/sendMessage"
 
-print("Bot started...")
 
-while True:
-    time.sleep(60)
+async def forward_to_bale(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message and update.message.text:
+        text = update.message.text
+
+        requests.post(
+            BALE_API_URL.format(BALE_TOKEN),
+            json={
+                "chat_id": BALE_CHAT_ID,
+                "text": text
+            }
+        )
+
+
+async def main():
+    application = Application.builder().token(TELEGRAM_TOKEN).build()
+    application.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, forward_to_bale)
+    )
+    await application.run_polling()
+
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
